1086964aab22ac3ace2ea9068b62e21877c0728a
Leonel Velez <leonel-14-velez@hotmail.com>
2021-01-20 22:20:26 -0500

    agegando rles, carrras y tesis

--- tables.sql
+++ tables.sql
@@ -27,16 +27,40 @@ USE mitesisug;
 
 
 
+--CARRERAS
+go
+IF NOT (EXISTS(SELECT * FROM sys.objects WHERE [object_id] = OBJECT_ID(N'[dbo].[CARRERAS]') AND [type]='U'))
+begin
+CREATE TABLE Carreras
+(
+	Id int identity(1,1),	
+	Nombre varchar(30) not null	
+);
+
+
+ALTER TABLE Carreras
+ADD CONSTRAINT pk_carreras primary key(Id);
+
+ALTER TABLE Carreras
+ADD CONSTRAINT uk_carreras unique(Nombre);
+end
+go
+
+
+
 --USUARIOS
 go
 IF NOT (EXISTS(SELECT * FROM sys.objects WHERE [object_id] = OBJECT_ID(N'[dbo].[USUARIOS]') AND [type]='U'))
 begin
+
+
 CREATE TABLE Usuarios 
 (
 	Id int identity(1,1),
 	Usuario varchar(10) not null,
 	Clave varchar(10) not null,
 	Nombre varchar(max) not null,
+	Carrera int not null,
 	Correo varchar(max) not null
 );
 
@@ -45,9 +69,13 @@ ALTER TABLE Usuarios
 ADD CONSTRAINT pk_usuario primary key(Id);
 
 ALTER TABLE Usuarios
+ADD CONSTRAINT fk_usuario_carrera foreign key(Carrera) references Carreras(Id);
+
+ALTER TABLE Usuarios
 ADD CONSTRAINT uk_usuario unique(Usuario);
 end
 go
+
 
 --ROLES
 go
@@ -70,6 +98,53 @@ end
 go
 
 
+--TIPO_TESIS
+go
+IF NOT (EXISTS(SELECT * FROM sys.objects WHERE [object_id] = OBJECT_ID(N'[dbo].[TIPOTESIS]') AND [type]='U'))
+begin
+
+CREATE TABLE TipoTesis
+(
+	Id int identity(1,1),	
+	Descripcion varchar(50) not null	
+);
+
+
+ALTER TABLE TipoTesis
+ADD CONSTRAINT pk_tipotesis primary key(Id);
+
+ALTER TABLE TipoTesis
+ADD CONSTRAINT uk_tipotesis_descripcion unique(Descripcion);
+end
+go
 
 
 
+--TESIS
+go
+IF NOT (EXISTS(SELECT * FROM sys.objects WHERE [object_id] = OBJECT_ID(N'[dbo].[TESIS]') AND [type]='U'))
+begin
+
+CREATE TABLE Tesis
+(
+	Id int identity(1,1),
+	Usuario int not null,
+	Titulo  varchar(max) not null,
+	Tipo int not null,
+	Descripcion varchar(max) not null	
+);
+
+
+ALTER TABLE Tesis
+ADD CONSTRAINT pk_tesis primary key(Id);
+
+ALTER TABLE Tesis
+ADD CONSTRAINT uk_tesis_titulo unique(Tipo);
+
+ALTER TABLE Tesis
+ADD CONSTRAINT fk_tesis_usuario foreign key(Usuario) references Usuarios(Id);
+
+ALTER TABLE Tesis
+ADD CONSTRAINT fk_tesis_tipo foreign key(Tipo) references TipoTesis(Id);
+end
+go
